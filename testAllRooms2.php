a239f6b96abc1b23a034442b60b3b6f4649042a3
Agfoghlaim <marieohara.gti@gmail.com>
2017-03-29 14:59:54 +0100

    started avail calendar

--- testAllRooms2.php
+++ testAllRooms2.php
@@ -13,25 +13,25 @@ try {
     $rooms = array('101', '102', '103', '104');
     $in = $_POST['date'] ;
     $out = $_POST['dateOut'] ;
-    $sql = "SELECT IF( COUNT(1),'No','Yes' ) AS Available
+    $sql = "SELECT IF( COUNT(1),'$rooms[0]: No','$rooms[0]: Yes' ) AS Available
             FROM bookings
             WHERE rm_no = '$rooms[0]' 
             AND booking_date < '$out' 
             AND checkout > '$in';
 
-            SELECT IF( COUNT(1),'No','Yes' ) AS Available
+            SELECT IF( COUNT(1),'$rooms[1]: No','$rooms[2]: Yes' ) AS Available
             FROM bookings
             WHERE rm_no = '$rooms[1]' 
             AND booking_date < '$out' 
             AND checkout > '$in';
 
-            SELECT IF( COUNT(1),'No','Yes' ) AS Available
+            SELECT IF( COUNT(1),'$rooms[2]: No','$rooms[2]: Yes' ) AS Available
             FROM bookings
             WHERE rm_no = '$rooms[2]' 
             AND booking_date < '$out' 
             AND checkout > '$in';
 
-            SELECT IF( COUNT(1),'No','Yes' ) AS Available
+            SELECT IF( COUNT(1),'$rooms[3]: No','$rooms[3]: Yes' ) AS Available
             FROM bookings
             WHERE rm_no = '$rooms[3]' 
             AND booking_date < '$out' 
@@ -59,7 +59,7 @@ try {
     do {
         $result = $dbc->store_result();
         $row = $result->fetch_assoc();
-        echo "<h2>{$row['Available']}</h2>";
+        echo "<h2>Room Number {$row['Available']}</h2>";
        
         $result->free();
     } while ($dbc->next_result());
